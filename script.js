45d3beb47c25d50ee8e3e8f9905302ff124f77d2
AnnaToro <annunadur@gmail.com>
2022-10-05 12:29:16 -0300

    Subiendo trabajo practico de JS

---
+++ script.js
@@ -0,0 +1,16 @@
+//#region 
+let alumna = ` Toro Anna`;
+let curso = `5°4`;
+let colegio = `Epet N°20`;
+//#endregion
+console.log(alumna);
+console.log(curso);
+console.log(colegio);
+
+//#region 
+var numero1 = `5`;
+let numero2 = `10`;
+const numero3 = `20`;
+//#endregion
+
+var suma = suma => numero1 + numero3;
