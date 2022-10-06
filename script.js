5388960981bafa40df2952786fc63668bb0d8104
AnnaToro <annunadur@gmail.com>
2022-10-06 20:21:35 -0300

    Subiendo el resto del codigo que se borro

--- script.js
+++ script.js
@@ -8,9 +8,24 @@ console.log(curso);
 console.log(colegio);
 
 //#region 
-var numero1 = `5`;
-let numero2 = `10`;
-const numero3 = `20`;
+var numero1 = 5;
+let numero2 = 10;
+const numero3 = 20;
+let resta = numero2 - numero1;
+var suma = numero1 + numero3;
+const division = numero3 / numero1;
+const multiplicacion = numero2 * numero3;
 //#endregion
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(suma);
+console.log(resta);
+console.log(division);
+console.log(multiplicacion);
 
-var suma = suma => numero1 + numero3;
+// arrays 
+
+let frutas = [`Sandia`, `Melon`, `frutilla`];
+let [a, b, c] = frutas;
+console.log(b, c, a);
